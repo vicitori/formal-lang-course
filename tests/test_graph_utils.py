c8e04acbc450c5853757ada1b6a03580467f11db
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2025-09-07 16:33:38 +0300

    Reformate according to pep8

--- tests/test_graph_utils.py
+++ tests/test_graph_utils.py
@@ -2,42 +2,49 @@ import pytest
 import pydot
 import os
 import networkx as nx
-from pathlib import Path
 from project.graph_utils import *
 import shutil
 
-############# GraphAnalyzer.nodes_cnt ############# 
+############# GraphAnalyzer.nodes_cnt #############
+
 
 def test_nodes_cnt__empty_graph():
     analyzer = GraphAnalyzer("empty")
     assert analyzer.get_nodes_cnt() == 0
 
+
 def test_nodes_cnt__graph_one_node_several_edges():
     analyzer = GraphAnalyzer("one_node_three_loops_has_attributes")
     assert analyzer.get_nodes_cnt() == 1
 
+
 def test_nodes_cnt__graph_from_dataset():
     analyzer = GraphAnalyzer("wine")
-    assert analyzer.get_nodes_cnt() == 733 # https://formallanguageconstrainedpathquerying.github.io/CFPQ_Data/graphs/data/wine.html#wine
+    # https://formallanguageconstrainedpathquerying.github.io/CFPQ_Data/graphs/data/wine.html#wine
+    assert analyzer.get_nodes_cnt() == 733
 
 
-############# GraphAnalyzer.edges_cnt ############# 
+############# GraphAnalyzer.edges_cnt #############
 
 def test_edges_cnt__empty_graph():
     analyzer = GraphAnalyzer("empty")
     assert analyzer.get_edges_cnt() == 0
 
+
 def test_edges_cnt__graph_with_loops():
     analyzer = GraphAnalyzer("one_node_three_loops_has_attributes")
     assert analyzer.get_edges_cnt() == 3
 
+
 def test_edges_cnt__multigraph():
     analyzer = GraphAnalyzer("two_nodes_several_edges")
     assert analyzer.get_edges_cnt() == 4
-    
+
+
 def test_edges_cnt__graph_from_dataset():
     analyzer = GraphAnalyzer("wine")
-    assert analyzer.get_edges_cnt() == 1839 # https://formallanguageconstrainedpathquerying.github.io/CFPQ_Data/graphs/data/wine.html#wine
+    # https://formallanguageconstrainedpathquerying.github.io/CFPQ_Data/graphs/data/wine.html#wine
+    assert analyzer.get_edges_cnt() == 1839
 
 
 ######### GraphAnalyzer.get_all_attributes ########
@@ -46,9 +53,11 @@ def test_get_all_attributes__no_attributes():
     analyzer = GraphAnalyzer("without_attributes")
     assert analyzer.get_all_attributes() == {}
 
+
 def test_get_all_attributes__two_attributes():
     analyzer = GraphAnalyzer("one_node_three_loops_has_attributes")
-    assert analyzer.get_all_attributes() == {"animal": {"cat", "dog"}, "flower": {"rose"}}
+    assert analyzer.get_all_attributes(
+    ) == {"animal": {"cat", "dog"}, "flower": {"rose"}}
 
 
 ############# GraphAnalyzer.get_labels ############
@@ -56,6 +65,7 @@ def test_get_all_attributes__two_attributes():
 def test_get_labels__has_attributes_no_labels():
     analyzer = GraphAnalyzer("one_node_three_loops_has_attributes")
     assert analyzer.get_labels() == {}
+
 
 def test_get_labels__has_not_only_labels():
     analyzer = GraphAnalyzer("has_attributes_including_labels")
@@ -67,8 +77,10 @@ def test_get_labels__has_not_only_labels():
 def test_get_graph_data__empty_graph():
     assert get_graph_data("empty") == GraphData(0, 0, {})
 
+
 def test_get_graph_data__normal_graph_with_different_attributes():
-    assert get_graph_data("has_attributes_including_labels") == GraphData(9, 9, {"1", "2", "3", "4"})
+    assert get_graph_data("has_attributes_including_labels") == GraphData(
+        9, 9, {"1", "2", "3", "4"})
 
 
 ############### get_two_cycles_graph ##############
@@ -76,12 +88,15 @@ def test_get_graph_data__normal_graph_with_different_attributes():
 TEST_CYCLIC_GRAPHS_PATH = CYCLIC_GRAPHS_PATH.parent.parent / "tests" / "tmp"
 
 # auxiliary for dot files reading
+
+
 def read_dot_graph(path) -> nx.MultiDiGraph:
     graphs = pydot.graph_from_dot_file(str(path))
     return nx.drawing.nx_pydot.from_pydot(graphs[0])
 
+
 def test_get_two_cycles_graph__file_creating():
-    try: 
+    try:
         path = TEST_CYCLIC_GRAPHS_PATH / "empty.dot"
         TEST_CYCLIC_GRAPHS_PATH.mkdir(exist_ok=True)
         get_two_cyclic_graph(3, 3, ("a", "b"), path)
@@ -108,4 +123,3 @@ def test_get_two_cycles_graph__invalid_nodes():
     finally:
         if TEST_CYCLIC_GRAPHS_PATH.exists():
             shutil.rmtree(TEST_CYCLIC_GRAPHS_PATH)
-        
