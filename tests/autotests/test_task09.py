498503883f056479d690859b795530bca859b7a8
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-11-17 00:20:08 +0300

    Turn on autotests

--- tests/autotests/test_task09.py
+++ tests/autotests/test_task09.py
@@ -15,10 +15,13 @@ from cfpq_concrete_cases import CaseCFPQ, CASES_CFPQ
 
 # Fix import statements in try block to run tests
 try:
-    from project.task6 import hellings_based_cfpq
-    from project.task7 import matrix_based_cfpq
-    from project.task8 import tensor_based_cfpq, cfg_to_rsm, ebnf_to_rsm
-    from project.task9 import gll_based_cfpq
+    from project.context_free_path_query import (
+        hellings_based_cfpq,
+        matrix_based_cfpq,
+        tensor_based_cfpq,
+        gll_based_cfpq
+    )
+    from project.rsm_utils import cfg_to_rsm, ebnf_to_rsm
 except ImportError:
     pytestmark = pytest.mark.skip("Task 9 is not ready to test!")
 
