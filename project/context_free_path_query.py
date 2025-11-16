1fa236d1cef37ac1f51ebf425e590527ffd5e940
Victoria Ostrovskaya <victoria.v.ostrovskaya@gmail.com>
2025-11-17 00:19:47 +0300

    Add gll_based_cfpq implementation

--- project/context_free_path_query.py
+++ project/context_free_path_query.py
@@ -202,3 +202,205 @@ def tensor_based_cfpq(
                 results.add((graph_fa.state_of_id[i], graph_fa.state_of_id[j]))
 
     return results
+
+def tensor_based_cfpq(
+    rsm: RecursiveAutomaton,
+    graph: nx.DiGraph,
+    start_nodes: set[int] = None,
+    final_nodes: set[int] = None,
+) -> set[tuple[int, int]]:
+    rsm_fa = AdjacencyMatrixFA(rsm_to_nfa(rsm))
+    graph_fa = AdjacencyMatrixFA(graph_to_nfa(graph, start_nodes, final_nodes))
+
+    g_size = graph_fa.state_cnt
+    changed = True
+
+    while changed:
+        changed = False
+        combined = intersect_automata(rsm_fa, graph_fa)
+        closure = combined.get_transitive_closure()
+        rows, cols = closure.nonzero()
+
+        for pos_r, pos_c in zip(rows, cols):
+            left_rsm = pos_r // g_size
+            left_graph = pos_r % g_size
+
+            right_rsm = pos_c // g_size
+            right_graph = pos_c % g_size
+
+            _, s_lbl = rsm_fa.state_of_id[left_rsm].value
+            _, t_lbl = rsm_fa.state_of_id[right_rsm].value
+
+            if s_lbl != t_lbl:
+                continue
+
+            if (
+                left_rsm not in rsm_fa.start_vector.nonzero()[0]
+                or right_rsm not in rsm_fa.final_vector.nonzero()[0]
+            ):
+                continue
+
+            mats = graph_fa.boolean_decomp_by_symbol
+            if s_lbl not in mats:
+                mats[s_lbl] = sparse.csr_matrix((g_size, g_size), dtype=bool)
+
+            if not mats[s_lbl][left_graph, right_graph]:
+                mats[s_lbl][left_graph, right_graph] = True
+                changed = True
+
+    results = set()
+
+    if rsm.initial_label not in graph_fa.boolean_decomp_by_symbol:
+        return results
+
+    mtx = graph_fa.boolean_decomp_by_symbol[rsm.initial_label]
+
+    for i in range(g_size):
+        for j in range(g_size):
+            if not mtx[i, j]:
+                continue
+
+            start_ok = (not start_nodes) or (graph_fa.state_of_id[i] in start_nodes)
+            final_ok = (not final_nodes) or (graph_fa.state_of_id[j] in final_nodes)
+
+            if start_ok and final_ok:
+                results.add((graph_fa.state_of_id[i], graph_fa.state_of_id[j]))
+
+    return results
+
+
+RsmState = tuple[object, object]  # (Symbol, State)
+GraphState = int
+GllConfig = tuple[RsmState, GraphState, int]
+
+
+def gll_based_cfpq(
+    rsm: RecursiveAutomaton,
+    graph: nx.DiGraph,
+    start_nodes: set[int] = None,
+    final_nodes: set[int] = None,
+) -> set[tuple[int, int]]:
+    def collect_rsm_transitions(_rsm) -> dict[RsmState, dict[object, set[RsmState]]]:
+        res: dict[RsmState, dict[object, set[RsmState]]] = {}
+        for box_label, box in _rsm.boxes.items():
+            dfa = box.dfa
+            net = dfa.to_networkx()
+            for u, v, data in net.edges(data=True):
+                lbl = data.get("label")
+                key = (box_label, u)
+                res.setdefault(key, {})
+                res[key].setdefault(lbl, set()).add((box_label, v))
+            for s in dfa.states:
+                res.setdefault((box_label, s), {})
+        return res
+
+    def collect_graph_transitions(
+        graph: nx.DiGraph,
+    ) -> dict[GraphState, dict[object, set[GraphState]]]:
+        res: dict[GraphState, dict[object, set[GraphState]]] = {}
+        for u, v, data in graph.edges(data=True):
+            lbl = data.get("label")
+            res.setdefault(u, {})
+            res[u].setdefault(lbl, set()).add(v)
+        for n in graph.nodes:
+            res.setdefault(n, {})
+        return res
+
+    if start_nodes is None:
+        start_nodes = set(graph.nodes)
+    if final_nodes is None:
+        final_nodes = set(graph.nodes)
+
+    rsm_trans = collect_rsm_transitions(rsm)
+    graph_trans = collect_graph_transitions(graph)
+
+    rsm_labels = set(rsm.boxes.keys())
+
+    gss_nodes: dict[int, dict] = {}
+    gss_edges: dict[int, list] = {}
+    next_gss_id = 0
+
+    def make_gss_node(rsm_state: RsmState, graph_state: GraphState) -> int:
+        nonlocal next_gss_id
+        for nid, info in gss_nodes.items():
+            if info["node"] == (rsm_state, graph_state):
+                return nid
+        nid = next_gss_id
+        next_gss_id += 1
+        gss_nodes[nid] = {"node": (rsm_state, graph_state), "returns": set()}
+        gss_edges[nid] = []
+        return nid
+
+    start_label = rsm.initial_label
+    start_dfa = rsm.boxes[start_label].dfa
+    start_state = (start_label, start_dfa.start_state)
+
+    init_configs: set[GllConfig] = set()
+    for sn in start_nodes:
+        gss_id = make_gss_node(start_state, sn)
+        cfg: GllConfig = (start_state, sn, gss_id)
+        init_configs.add(cfg)
+
+    work: set[GllConfig] = set(init_configs)
+    seen: set[GllConfig] = set(init_configs)
+
+    def step_term(cfg: GllConfig) -> set[GllConfig]:
+        rsm_state, gstate, gss_id = cfg
+        out = set()
+        rsm_map = rsm_trans.get(rsm_state, {})
+        graph_map = graph_trans.get(gstate, {})
+        common_labels = set(rsm_map.keys()) & set(graph_map.keys())
+        for lbl in common_labels:
+            for nr in rsm_map[lbl]:
+                for ng in graph_map[lbl]:
+                    out.add((nr, ng, gss_id))
+        return out
+
+    def step_nonterm(cfg: GllConfig) -> set[GllConfig]:
+        rsm_state, gstate, gss_id = cfg
+        out = set()
+        candidate_labels = set(rsm_trans.get(rsm_state, {}).keys()) & rsm_labels
+        for label in candidate_labels:
+            callee_dfa = rsm.boxes[label].dfa
+            callee_start = (label, callee_dfa.start_state)
+            callee_gss_id = make_gss_node(callee_start, gstate)
+
+            for return_rsm_state in rsm_trans[rsm_state][label]:
+                if gss_nodes[callee_gss_id]["returns"]:
+                    for ret_graph_state in gss_nodes[callee_gss_id]["returns"]:
+                        out.add((return_rsm_state, ret_graph_state, gss_id))
+                gss_edges[callee_gss_id].append((gss_id, return_rsm_state))
+            out.add((callee_start, gstate, callee_gss_id))
+        return out
+
+    def step_return(cfg: GllConfig) -> set[GllConfig]:
+        rsm_state, gstate, gss_id = cfg
+        out = set()
+        box_label, dfa_state = rsm_state
+        dfa = rsm.boxes[box_label].dfa
+        if dfa_state not in dfa.final_states:
+            return out
+        gss_nodes[gss_id]["returns"].add(gstate)
+        for parent_id, return_rsm_state in gss_edges[gss_id]:
+            out.add((return_rsm_state, gstate, parent_id))
+        return out
+
+    while work:
+        cfg = work.pop()
+        new_cfgs = set()
+        new_cfgs |= step_term(cfg)
+        new_cfgs |= step_nonterm(cfg)
+        new_cfgs |= step_return(cfg)
+
+        for nc in new_cfgs:
+            if nc not in seen:
+                seen.add(nc)
+                work.add(nc)
+
+    result: set[tuple[int, int]] = set()
+    for init in init_configs:
+        _, init_graph_state, init_gss_id = init
+        for ret in gss_nodes[init_gss_id]["returns"]:
+            if ret in final_nodes:
+                result.add((init_graph_state, ret))
+    return result
